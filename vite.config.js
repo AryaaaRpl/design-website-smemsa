6dc73d2f4d41512752b0c3e74f9675cecc6050f5
Dannys02 <webdannys@gmail.com>
2026-09-09 11:36:32 +0700

    merapikan stuktur css per halaman

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,21 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/pages/index.css',
+                'resources/css/pages/visi-misi.css',
+                'resources/css/pages/berita.css',
+                'resources/css/pages/bkk.css',
+                'resources/css/pages/ekstrakurikuler.css',
+                'resources/css/pages/fasilitas.css',
+                'resources/css/pages/guru.css',
+                'resources/css/pages/lsp.css',
+                'resources/css/pages/peta-kampus.css',
+                'resources/css/pages/prestasi.css',
+                'resources/css/pages/spmb.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
